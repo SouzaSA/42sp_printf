e52efe319a850b9d515651d75c1e0607200b5b0b
sde-alva <sde-alva@student.42sp.org.br>
2021-08-18 10:24:12 -0300

    corrected a bug on % descriptor print

--- ft_printf_flags.c
+++ ft_printf_flags.c
@@ -6,7 +6,7 @@
 /*   By: sde-alva <sde-alva@student.42sp.org.br>    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/08/08 22:48:13 by sde-alva          #+#    #+#             */
-/*   Updated: 2021/08/14 21:56:49 by sde-alva         ###   ########.fr       */
+/*   Updated: 2021/08/18 10:04:23 by sde-alva         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -39,7 +39,7 @@ int	ft_func_selector(char const **str, va_list args)
 	if (**str == 'X')
 		i = ft_printf_xX(va_arg(args, unsigned long), &flags, 1);
 	if (**str == '%')
-		i = ft_printf_put("\%", &flags);
+		i = write(1, "%", 1);
 	return (i);
 }
 
