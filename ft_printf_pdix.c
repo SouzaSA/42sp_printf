f64518434e03923f708d82fc76c148ca281d36ca
sde-alva <sde-alva@student.42sp.org.br>
2021-08-18 11:32:18 -0300

    added null pointer behavior on p identifier

--- ft_printf_pdix.c
+++ ft_printf_pdix.c
@@ -6,7 +6,7 @@
 /*   By: sde-alva <sde-alva@student.42sp.org.br>    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/08/10 18:12:09 by sde-alva          #+#    #+#             */
-/*   Updated: 2021/08/18 11:20:42 by sde-alva         ###   ########.fr       */
+/*   Updated: 2021/08/18 11:23:01 by sde-alva         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -20,9 +20,7 @@ int	ft_printf_pointer(unsigned long long paddr, t_printf_flags *flags)
 	char	*str_addr;
 	char	*tmp;
 
-	tmp = "0";
-	if (paddr)
-		tmp = ft_ulltoa_base(paddr, 16);
+	tmp = ft_ulltoa_base(paddr, 16);
 	str_addr = ft_strjoin_mod("0x", tmp);
 	free(tmp);
 	tmp = NULL;
